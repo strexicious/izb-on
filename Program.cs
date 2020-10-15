d6800330bcf46c84b5d89f80ce2bd71385fa50ca
Strexicious <strexicious@gmail.com>
2020-10-15 09:09:47 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace apur_on
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
